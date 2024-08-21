cf05d5fe26f33acdbd51405faff9dc5676aec13e
hllj <vanhop3499@gmail.com>
2024-08-22 00:59:57 +0700

    fix prompt error

--- prompts.py
+++ prompts.py
@@ -12,18 +12,15 @@ retrieval task in one sentence. Do not explain yourself or output anything else.
 """
 
 synthetic_sample_text_retrieval_template = """You have been assigned a retrieval task: {task}
-Your mission is to write one text retrieval example for this task in JSON format. The JSON object must contain the following
-keys:
+Your mission is to write one text retrieval example for this task in JSON format. The JSON object must contain the following keys:
 - "user_query": a string, a random user search query specified by the retrieval task.
 - "positive_document": a string, a relevant document for the user query.
 - "hard_negative_document": a string, a hard negative document that only appears relevant to the query.
 Please adhere to the following guidelines:
 - The "user_query" should be {query_type}, {query_length}, {clarity}, and diverse in topic.
-- All documents must be created independent of the query. Avoid copying the query verbatim. It’s acceptable if some parts of
-the "positive_document" are not topically related to the query.
+- All documents must be created independent of the query. Avoid copying the query verbatim. It’s acceptable if some parts of the "positive_document" are not topically related to the query.
 - All documents should be at least {num_words} words long.
-- The "hard_negative_document" contains some useful information, but it should be less useful or comprehensive compared
-to the "positive_document".
+- The "hard_negative_document" contains some useful information, but it should be less useful or comprehensive compared to the "positive_document".
 - Both the query and documents should be in {language}.
 - Do not provide any explanation in any document on why it is relevant or not relevant to the query.
 - Both the query and documents require {difficulty} level education to understand.
@@ -39,8 +36,7 @@ text classification task in one sentence. Do not explain yourself or output anyt
 """
 
 synthetic_sample_text_classification_template = """You have been assigned a text classification task: {task}
-Your mission is to write one text classification example for this task in JSON format. The JSON object must contain the
-following keys:
+Your mission is to write one text classification example for this task in JSON format. The JSON object must contain the following keys:
 - "input_text": a string, the input text specified by the classification task.
 - "label": a string, the correct label of the input text.
 - "misleading_label": a string, an incorrect label that is related to the task.
@@ -48,8 +44,7 @@ Please adhere to the following guidelines:
 - The "input_text" should be {num_words} words and diverse in expression.
 - The "misleading_label" must be a valid label for the given task, but not as appropriate as the "label" for the "input_text".
 - The values for all fields should be in {language}.
-- Avoid including the values of the "label" and "misleading_label" fields in the "input_text", that would make the task too
-easy.
+- Avoid including the values of the "label" and "misleading_label" fields in the "input_text", that would make the task too easy.
 - The "input_text" is {clarity} and requires {difficulty} level education to comprehend.
 Your output must always be a JSON object only, do not explain yourself or output anything else. Be creative!
 """
